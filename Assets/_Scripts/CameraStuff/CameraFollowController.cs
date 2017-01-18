f2b26a66062682be1e424ef0720a97b08a40a750
chancetofreeze <temalamorsa@hotmail.com>
2017-01-18 07:22:33 -0500

    Ground colors, camera improvements, 2.5d
    2.5D world added, ground tiles now automatically change colors on a
    timestep. the "story" has been decided. the game takes place in a disco
    a dude stole your girl and you gotta outdance his thugs and roast that
    jive turkey with your funkalicious dance moves.

--- Assets/_Scripts/CameraStuff/CameraFollowController.cs
+++ Assets/_Scripts/CameraStuff/CameraFollowController.cs
@@ -8,11 +8,33 @@ public class CameraFollowController : MonoBehaviour
     //this creates an effect where the player sees further ahead of themselves when standing still and a roughly equal distance on either side when moving
     void Update()
     {
-        float lerpPathLength = Vector3.Distance(this.transform.position, GlobalsManager.Instance.CAMERA_MAIN_FOLLOW_ACTOR.position);
+        //this will eventually become code to give the player more precise control over the camera panning. so when they get closer to the edge of the screen the camera moves faster to keep
+        //the forward direction in view.
+        //if (Vector3.Distance(GlobalsManager.Instance.CURRENT_CAMERA_TRANSFORM.position, GlobalsManager.Instance.CAMERA_MAIN_FOLLOW_ACTOR.position) < GlobalsManager.Instance.CAMERA_SPEEDUP_DISTANCE)
+        //{
+        //    float lerpPathLength = Vector3.Distance(GlobalsManager.Instance.CURRENT_CAMERA_TRANSFORM.position, GlobalsManager.Instance.CAMERA_MAIN_FOLLOW_ACTOR.position);
+        //    float distCoveredInTimeStep = Time.deltaTime * GlobalsManager.Instance.CAMERA_PAN_SPEED;
+        //    float totalLerpPath = distCoveredInTimeStep / lerpPathLength;
+
+        //    float newXPosition = Mathf.Lerp(GlobalsManager.Instance.CURRENT_CAMERA_TRANSFORM.position.x, GlobalsManager.Instance.CAMERA_MAIN_FOLLOW_ACTOR.position.x, totalLerpPath);
+        //    GlobalsManager.Instance.CURRENT_CAMERA_TRANSFORM.position = new Vector3(newXPosition, GlobalsManager.Instance.CURRENT_CAMERA_TRANSFORM.position.y, GlobalsManager.Instance.CURRENT_CAMERA_TRANSFORM.position.z);
+        //}
+        //else
+        //{
+        //    float lerpPathLength = Vector3.Distance(GlobalsManager.Instance.CURRENT_CAMERA_TRANSFORM.position, GlobalsManager.Instance.CAMERA_MAIN_FOLLOW_ACTOR.position);
+        //    float distCoveredInTimeStep = Time.deltaTime * GlobalsManager.Instance.CAMERA_PAN_SPEED * GlobalsManager.Instance.CAMERA_SPEEDUP_VALUE;
+        //    float totalLerpPath = distCoveredInTimeStep / lerpPathLength;
+
+        //    float newXPosition = Mathf.Lerp(GlobalsManager.Instance.CURRENT_CAMERA_TRANSFORM.position.x, GlobalsManager.Instance.CAMERA_MAIN_FOLLOW_ACTOR.position.x, totalLerpPath);
+        //    GlobalsManager.Instance.CURRENT_CAMERA_TRANSFORM.position = new Vector3(newXPosition, GlobalsManager.Instance.CURRENT_CAMERA_TRANSFORM.position.y, GlobalsManager.Instance.CURRENT_CAMERA_TRANSFORM.position.z);
+        //}
+
+        float lerpPathLength = Vector3.Distance(GlobalsManager.Instance.CURRENT_CAMERA_TRANSFORM.position, GlobalsManager.Instance.CAMERA_MAIN_FOLLOW_ACTOR.position);
         float distCoveredInTimeStep = Time.deltaTime * GlobalsManager.Instance.CAMERA_PAN_SPEED;
         float totalLerpPath = distCoveredInTimeStep / lerpPathLength;
 
         float newXPosition = Mathf.Lerp(GlobalsManager.Instance.CURRENT_CAMERA_TRANSFORM.position.x, GlobalsManager.Instance.CAMERA_MAIN_FOLLOW_ACTOR.position.x, totalLerpPath);
-        GlobalsManager.Instance.CURRENT_CAMERA_TRANSFORM.position = new Vector3(newXPosition, GlobalsManager.Instance.CURRENT_CAMERA_TRANSFORM.position.y, GlobalsManager.Instance.CURRENT_CAMERA_TRANSFORM.position.z);
+        float newYPosition = Mathf.Lerp(GlobalsManager.Instance.CURRENT_CAMERA_TRANSFORM.position.y, GlobalsManager.Instance.CAMERA_MAIN_FOLLOW_ACTOR.position.y + GlobalsManager.Instance.CAMERA_Y_OFFSET, totalLerpPath);
+        GlobalsManager.Instance.CURRENT_CAMERA_TRANSFORM.position = new Vector3(newXPosition, newYPosition, GlobalsManager.Instance.CURRENT_CAMERA_TRANSFORM.position.z);
     }	
 }
---
+++ Assets/_Scripts/LevelDesignStuff/CharacterMatchFloorColor.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMatchFloorColor : MonoBehaviour {
+
+	void FixedUpdate()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(GlobalsManager.Instance.PLAYER_TRANSFORM.position, Vector3.down, Mathf.Infinity, GlobalsManager.Instance.GROUND_LAYER);
+        if (hit.collider != null)
+        {
+            Debug.Log(hit.collider.gameObject.name + " hit");
+            Color color = new Color(hit.collider.gameObject.GetComponent<Renderer>().material.GetColor("_Color").r, hit.collider.gameObject.GetComponent<Renderer>().material.GetColor("_Color").g, hit.collider.gameObject.GetComponent<Renderer>().material.GetColor("_Color").b, 1);
+            GlobalsManager.Instance.PLAYER_MATERIAL.SetColor("_MyColor", color); 
+        }
+    }
+}
